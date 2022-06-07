445ebc333556f568482cd25bebf8320105c2f814
flociaglia <flociaglia@u.boisestate.edu>
2022-06-07 16:22:26 +0000

    reorganized repo

---
+++ yolov5-benchmark/parse_eval.py
@@ -0,0 +1,92 @@
+import json
+import yaml
+import argparse
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-l", "--loc", required=True, help="data file location")
+args = vars(ap.parse_args())
+loc = args["loc"] 
+
+
+with open(loc + "/data.yaml", 'r') as stream:
+    class_names = yaml.safe_load(stream)['names']
+
+with open("val_eval.txt", "r") as f:
+    lines = f.readlines()
+    eval_lines = []
+    for line in lines:
+        entries = line.split(" ")
+        entries = list(filter(lambda val: val !=  "", entries))
+        entries = [e.strip("\n") for e in entries]
+        start_class = False
+        for e in entries:
+            if e == "all":
+                if "(AP)" not in entries:
+                    if "(AR)" not in entries:
+                        #parse all
+                        eval = {}
+                        eval["class"] = entries[0]
+                        eval["images"] = entries[1]
+                        eval["targets"] = entries[2]
+                        eval["precision"] = entries[3]
+                        eval["recall"] = entries[4]
+                        eval["map50"] = entries[5]
+                        eval["map95"] = entries[6]
+                        eval_lines.append(eval)
+
+            if e in class_names:
+                eval = {}
+                eval["class"] = entries[0]
+                eval["images"] = entries[1]
+                eval["targets"] = entries[2]
+                eval["precision"] = entries[3]
+                eval["recall"] = entries[4]
+                eval["map50"] = entries[5]
+                eval["map95"] = entries[6]
+                eval_lines.append(eval)
+
+print("-- This is the result:")
+print(eval_lines)
+#parsing stats for paper
+
+# with open("results_verbose_valid.json", 'w+') as f:
+#     json.dump(eval_lines, f)
+
+# with open("test_eval.txt", "r") as f:
+#     lines = f.readlines()
+#     eval_lines = []
+#     for line in lines:
+#         entries = line.split(" ")
+#         entries = list(filter(lambda val: val !=  "", entries))
+#         entries = [e.strip("\n") for e in entries]
+#         start_class = False
+#         for e in entries:
+#             if e == "all":
+#                 if "(AP)" not in entries:
+#                     if "(AR)" not in entries:
+#                         #parse all
+#                         eval = {}
+#                         eval["class"] = entries[0]
+#                         eval["images"] = entries[1]
+#                         eval["targets"] = entries[2]
+#                         eval["precision"] = entries[3]
+#                         eval["recall"] = entries[4]
+#                         eval["map50"] = entries[5]
+#                         eval["map95"] = entries[6]
+#                         eval_lines.append(eval)
+
+#             if e in class_names:
+#                 eval = {}
+#                 eval["class"] = entries[0]
+#                 eval["images"] = entries[1]
+#                 eval["targets"] = entries[2]
+#                 eval["precision"] = entries[3]
+#                 eval["recall"] = entries[4]
+#                 eval["map50"] = entries[5]
+#                 eval["map95"] = entries[6]
+#                 eval_lines.append(eval)
+
+# with open("/results_verbose_test.json", 'w+') as f:
+#     json.dump(eval_lines, f)
---
+++ yolov5-benchmark/set_up_dataset.py
@@ -0,0 +1,38 @@
+from roboflow import Roboflow
+import os 
+import argparse
+
+def main():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+
+    # ap.add_argument("-k", "--api_key", required=True, help="api key") 
+    ap.add_argument("-w", "--workspace", required=True, help="workspace")
+    ap.add_argument("-p", "--project", required=True, help="project")
+    ap.add_argument("-v", "--version", required=True, help="version")
+    ap.add_argument("-d", "--download", required=True, help="download")
+
+    # parses command line arguments 
+    args = vars(ap.parse_args())
+
+    # api_key = args["api_key"] 
+    workspace = args["workspace"] 
+    project = args["project"]
+    version = args["version"]
+    download = args["download"]
+
+    print("-- ", workspace, project, version, download)
+
+    rf = Roboflow(api_key="ddkQGNIW95EfGbudHOeh")
+    project = rf.workspace(workspace).project(project)
+    dataset = project.version(int(version)).download(download)
+    print(" *** ", dataset.location)
+    with open('../loc.txt', 'w') as f:
+        f.write(dataset.location)
+
+
+    return 
+
+
+if __name__ == "__main__":
+    main()
