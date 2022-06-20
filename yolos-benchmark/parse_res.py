69454fed2ad2503e47121ae1489751a19eb491b7
flociaglia <flociaglia@u.boisestate.edu>
2022-06-20 13:36:57 +0000

    Fixed the mAP value saving for yolos

--- yolos-benchmark/parse_res.py
+++ yolos-benchmark/parse_res.py
@@ -1,7 +1,15 @@
 import os
+import argparse
 from os import path
 
 def main():
+
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-l", "--loc", required=True, help="location")
+    args = vars(ap.parse_args())
+    loc = args["loc"] 
+
     # read the file with stdout data
     res_file = open("yolos_res.txt", "r")
     # get list of lines
@@ -13,33 +21,15 @@ def main():
     for line in lines:
         if "IoU=0.50 " in line.strip("\n"):
             # Delete "line2" from new_file
-            
-            
             res_file.write(line)
-            res = "YOLOs:" , line[-6:] # value that we want to save to res file
+            res = loc, ": ", line[-6:] # value that we want to save to res file
 
     res_file.close()
     
-    if path.exists("../mAP_results.txt"):
-        print("We should get in here")
-        mAP_file = open("../mAP_results.txt", "r")
-        # get list of lines
-        lines = mAP_file.readlines()
-        mAP_file.close()
-
-        mAP_file = open("../mAP_results.txt", "w")
-        for line in lines:
-            if "YOLOs" not in line.strip("\n"):
-                mAP_file.write(line)
-        mAP_file.write("\n")
-        mAP_file.write(''.join(res))
-
-        mAP_file.close()
-
-    else:
+    with open('mAP_s.txt', 'a') as f:
+        f.write(''.join(res))
+        f.write("\n")
     
-        with open('../mAP_results.txt', 'w') as f:
-            f.write(''.join(res))
 
 
 
