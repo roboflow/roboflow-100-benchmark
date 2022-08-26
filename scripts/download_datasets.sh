30051e1d0299321086280f83f224f940cd6bfdb3
fra <francesco.zuppichini@gmail.com>
2022-08-26 15:14:40 +0200

    download_datasets.sh

---
+++ scripts/download_datasets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+input="$(pwd)/datasets_links.txt"
+
+while getopts f:l: flag
+do
+    case "${flag}" in
+        f) format=${OPTARG};;
+        l) location=${OPTARG};;
+    esac
+done
+# default values
+format=${format:-coco}
+location=${location:-$(pwd)/rf100}
+
+echo "Starting downloading RF100..."
+
+for link in $(cat $input)
+do
+    attributes=$(python3 $(pwd)/scripts/parse_dataset_link.py  -l $link)
+     
+    project=$(echo $attributes | cut -d' ' -f 3)
+    version=$(echo $attributes | cut -d' ' -f 4)
+
+    python3 $(pwd)/scripts/download_dataset.py -p $project -v $version
+
+done
+
+echo "Done!"
