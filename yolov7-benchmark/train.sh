63c1d53831b42e2cdbf45714f10a4a5db19669d4
Francesco Saverio Zuppichini <francesco.zuppichini@gmail.com>
2022-10-07 15:48:36 +0200

    Update train.sh

--- yolov7-benchmark/train.sh
+++ yolov7-benchmark/train.sh
@@ -16,19 +16,22 @@ fi
 
 cd $(pwd)/yolov7-benchmark/
 
-if [ ! -f "yolov7.pt" ] ; then
-    wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt
-fi
-
 cd yolov7/
+
+if [ ! -f "yolov7_training.pt" ] ; then
+    wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7_training.pt
+fi
 
 for dataset in $(ls $datasets)
 do
     dataset=$datasets/$dataset
     echo "Training on $dataset"
-    python train.py --img 640 --batch 16 --epochs 100 --name $dataset/results --data $dataset/data.yaml  --weights ./yolov7.pt 
-    python test.py --data $dataset/data.yaml --img 640 --batch 16 --weights $dataset/results/weights/best.pt --name  $dataset --exist-ok |& tee $dataset/val_eval.txt 
-    python ../parse_eval.py -i $dataset/val_eval.txt -l $dataset -o $dir/final_eval.txt
+    if [ ! -d "$dataset/results" ] ;
+    then
+        python train.py --img 640 --batch 16 --epochs 100 --name $dataset/results --data $dataset/data.yaml  --weights 'yolov7_training.pt' |& tee $dataset/logs.txt 
+        python test.py --data $dataset/data.yaml --img 640 --batch 16 --weights $dataset/results/weights/best.pt --name  $dataset --exist-ok |& tee $dataset/val_eval.txt 
+        python ../parse_eval.py -i $dataset/val_eval.txt -l $dataset -o $dir/final_eval.txt
+    fi
 done
 
 echo "Done training all the datasets with YOLOv7!"
